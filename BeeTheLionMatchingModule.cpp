cf1c6df7485223b991f18bfc9b7891adb052e2d0
superjoe <treepop.sunpetchniyom@nectec.or.th>
2011-08-03 18:08:55 +0700

    There is a bug that make compilation was error.

---
+++ BeeTheLionMatchingModule.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include <opencv2\core\core.hpp>
+#include <opencv2\highgui\highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	string strDirFlowerDB = "flowerPicDB\\";
+	string strFNameFlowerDB = strDirFlowerDB + "files.txt";
+	string strFNameFlower;
+	ifstream inFile;
+	int count = 0;
+
+	inFile.open(strFNameFlowerDB);
+	while(inFile >> strFNameFlower)
+	{
+		count++;
+	}
+	inFile.close();
+	cout << "Number of flower photo = " << count << endl;
+
+	Mat *imgFlower = new Mat[count];
+	/*inFile.open(strFNameFlowerDB);
+	while(inFile >> strFNameFlower)
+	{
+		cout << strFNameFlower << endl;
+		imgFlower = imread(strDirFlowerDB + strFNameFlower);
+		namedWindow(strFNameFlower);
+		imshow(strFNameFlower,imgFlower);
+	}
+	inFile.close();*/
+
+	waitKey(0);
+
+	return 0;
+}
